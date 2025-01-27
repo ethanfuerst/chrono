d4c5f120cdad4bb058c3a2b6d089c43d924f05e7
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2025-01-27 11:14:51 -0500

    adding raw data dump for troy

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import modal
 from modal.runner import deploy_app
 
 from nba_data_lakehouse.etl import update_nba_data
+from raw_box_office_for_troy.etl import load_data_to_sheet, pull_data_from_s3
 from utils import setup_logging
 
 setup_logging()
@@ -33,13 +34,38 @@ def nba_data(full_refresh: bool = False):
     logging.info('NBA data updated.')
 
 
+@app.function(
+    image=modal_image,
+    schedule=modal.Cron('5 4 * * *'),
+    secrets=[CHRONO_SECRETS],
+    retries=modal.Retries(
+        max_retries=3,
+        backoff_coefficient=1.0,
+        initial_delay=60.0,
+    ),
+    mounts=[
+        modal.Mount.from_local_dir(
+            'raw_box_office_for_troy/assets/',
+            remote_path='/root/raw_box_office_for_troy/assets/',
+        ),
+    ],
+)
+def box_office_data():
+    logging.info('Pulling box office data from S3.')
+    df = pull_data_from_s3()
+    logging.info('Box office data pulled from S3.')
+    logging.info('Loading box office data to sheet.')
+    load_data_to_sheet(df)
+    logging.info('Box office data loaded to sheet.')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Run specific functions of the chrono app.'
     )
     parser.add_argument(
         '--function',
-        choices=['nba_data'],
+        choices=['nba_data', 'box_office_data'],
         required=True,
         help='The function to run.',
     )
@@ -53,13 +79,13 @@ if __name__ == '__main__':
 
     if args.function == 'nba_data':
         if args.full_refresh:
-            logging.info(
-                'Running nba_data function locally with full refresh: %s',
-                args.full_refresh,
-            )
+            logging.info('Running nba_data function locally with full refresh.')
             nba_data.local(full_refresh=True)
         else:
             logging.info('Running nba_data function locally with incremental run.')
             nba_data.local(full_refresh=False)
+    elif args.function == 'box_office_data':
+        logging.info('Running box_office_data function locally.')
+        box_office_data.local()
 
     logging.info('Chrono finished running locally.')
---
+++ raw_box_office_for_troy/etl.py
@@ -0,0 +1,117 @@
+import json
+import logging
+import os
+
+from gspread import Worksheet, service_account_from_dict
+from pandas import DataFrame
+
+from utils import DuckDBConnection, setup_logging
+
+setup_logging()
+
+S3_DATE_FORMAT = '%Y-%m-%d'
+
+
+def pull_data_from_s3() -> DataFrame:
+    duckdb_con = DuckDBConnection(
+        s3_access_key_id=os.getenv('BOX_OFFICE_TRACKING_S3_ACCESS_KEY_ID'),
+        s3_secret_access_key=os.getenv('BOX_OFFICE_TRACKING_S3_SECRET_ACCESS_KEY'),
+    ).get_connection()
+
+    duckdb_con.execute(
+        f'''
+        create or replace table box_office_mojo_dump as (
+            with all_data as (
+                select
+                    *
+                    , split_part(split_part(filename, 'release_year=', 2), '/', 1) as year_part_from_s3
+                    , strptime(split_part(split_part(filename, 'scraped_date=', 2), '/', 1), '{S3_DATE_FORMAT}') as scraped_date_from_s3
+                from read_parquet('s3://box-office-tracking/release_year=*/scraped_date=*/data.parquet', filename=true)
+            )
+
+            select
+                "Release Group" as title
+                , coalesce(try_cast(replace("Worldwide"[2:], ',', '') as integer), 0) as revenue
+                , coalesce(try_cast(replace("Domestic"[2:], ',', '') as integer), 0) as domestic_rev
+                , coalesce(try_cast(replace("Foreign"[2:], ',', '') as integer), 0) as foreign_rev
+                , scraped_date_from_s3 as loaded_date
+                , year_part_from_s3 as year_part
+            from all_data
+        );
+
+        create or replace table drafter as (
+            select
+                round
+                , overall as overall_pick
+                , name
+                , movie
+            from read_csv_auto('raw_box_office_for_troy/assets/box_office_draft.csv')
+        );
+
+        create or replace table manual_adds as (
+            select
+                title
+                , try_cast(revenue as integer) as revenue
+                , try_cast(domestic_rev as integer) as domestic_rev
+                , try_cast(foreign_rev as integer) as foreign_rev
+                , try_cast(release_date as date) as first_seen_date
+            from read_csv_auto('raw_box_office_for_troy/assets/manual_adds.csv')
+        );
+
+        create or replace table raw_box_office_for_troy as (
+            with raw_data_and_manual_adds as (
+                select
+                    title
+                    , revenue
+                    , domestic_rev
+                    , foreign_rev
+                    , loaded_date
+                from box_office_mojo_dump
+                where year_part = 2025
+                and title not in (select distinct title from manual_adds)
+
+                union all
+
+                select
+                    title
+                    , revenue
+                    , domestic_rev
+                    , foreign_rev
+                    , first_seen_date as loaded_date
+                from manual_adds
+            )
+
+            select
+                raw_.title
+                , raw_.revenue
+                , raw_.domestic_rev
+                , raw_.foreign_rev
+                , raw_.loaded_date
+                , drafter.round as round_drafted
+                , drafter.overall_pick
+                , drafter.name
+            from raw_data_and_manual_adds as raw_
+            left join drafter
+                on raw_.title = drafter.movie
+        );
+        '''
+    )
+
+    row_count = 'select count(*) from raw_box_office_for_troy'
+    logging.info(
+        f'Read {duckdb_con.query(row_count).fetchnumpy()["count_star()"][0]} rows with query from s3 bucket'
+    )
+
+    df = duckdb_con.query('select * from raw_box_office_for_troy').fetchdf().astype(str)
+    return df
+
+
+def load_data_to_sheet(df: DataFrame):
+    credentials_dict = json.loads(
+        os.getenv('BOX_OFFICE_TRACKING_GSPREAD_CREDENTIALS').replace('\n', '\\n')
+    )
+    gc = service_account_from_dict(credentials_dict)
+    sh = gc.open('2025 Raw Box Office Draft Data for Troy')
+    worksheet = sh.worksheet('raw_data')
+    worksheet.clear()
+    worksheet.update([df.columns.tolist()] + df.values.tolist())
